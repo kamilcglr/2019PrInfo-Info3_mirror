fa9935d4a8e6cf2a97a6ad2de0acb51ca7647cb6
Kamil CAGLAR <kamilcaglar.contact@gmail.com>
2019-10-30 22:36:07 +0100

    feat: #18 add verification to search and calls another tab

--- src/main/java/fr/tse/ProjetInfo3/mwp/controller/MainController.java
+++ src/main/java/fr/tse/ProjetInfo3/mwp/controller/MainController.java
@@ -39,7 +39,12 @@ public class MainController {
 
     }
 
-    public void goToSearchPane(){
+    public void goToUserPane(){
+        tabPane.getSelectionModel().select(userTabFromMain);;
+    }
+
+    public void goToHashtagPane(){
+        //TODO
         tabPane.getSelectionModel().select(userTabFromMain);;
     }
 }
