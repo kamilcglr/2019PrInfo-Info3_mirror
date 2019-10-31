f255f97d71a8d3fe634417da1a5a510f32960d3a
Kamil CAGLAR <kamilcaglar.contact@gmail.com>
2019-10-31 11:42:37 +0100

    feat: #18 redirect on specific page

--- src/main/java/fr/tse/ProjetInfo3/mwp/controller/MainController.java
+++ src/main/java/fr/tse/ProjetInfo3/mwp/controller/MainController.java
@@ -1,13 +1,13 @@
 package fr.tse.ProjetInfo3.mwp.controller;
 
 import com.jfoenix.controls.JFXTabPane;
-import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
 
 /**
  * @author Kamil CAGLAR
  * Controller of the main window, all user interactions whith the main windows (not the tabs) are handled here
+ * Subcontrollers (searchTabController, userTabController, userTabFromMain) are called here
  */
 public class MainController {
     /*
@@ -24,12 +24,14 @@ public class MainController {
     @FXML
     private Tab userTabFromMain;
     @FXML
+    private Tab hashtagTabFromMain;
+    @FXML
     private Tab piTabFromMain;
     @FXML
     private Tab searchTabFromMain;
 
 
-    /*This function is launched when Mainwindows is launched */
+    /*This function is launched when Mainwindow is launched */
     @FXML
     private void initialize() {
         /*the controller can be used in other Tabs*/
@@ -44,7 +46,6 @@ public class MainController {
     }
 
     public void goToHashtagPane(){
-        //TODO
-        tabPane.getSelectionModel().select(userTabFromMain);;
+        tabPane.getSelectionModel().select(hashtagTabFromMain);;
     }
 }
