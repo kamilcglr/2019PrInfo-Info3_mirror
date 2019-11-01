b42ea87c8d33c35b6071f43860ce61a257491524
Taha Alami Idrissi <miala69@hotmail.fr>
2019-11-01 12:09:36 +0100

    refactor : #9 create the corps of the interface and class

--- src/main/java/fr/tse/ProjetInfo3/mwp/dao/HashtagInterface.java
+++ src/main/java/fr/tse/ProjetInfo3/mwp/dao/HashtagInterface.java
@@ -1,5 +1,16 @@
 package fr.tse.ProjetInfo3.mwp.dao;
 
+
+/**
+ * @author ALAMI IDRISSI Taha
+ *	this interface is a contract that's going to be implemented by the Hashtag class 
+ *  As we work with interfaces in DAO pattern, it also emphasizes the style of “work with interfaces instead of implementation” 
+ *  which is an excellent OOPs style of programming. 
+ *  Object after that are going to be created like this : InterestPointInterface ip = new InterestPoint();
+ */
 public interface HashtagInterface {
+	
+	
+	
 
 }
