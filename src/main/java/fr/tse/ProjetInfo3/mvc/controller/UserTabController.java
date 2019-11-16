3ecb24c65004538e940beb0738b59fa53e8392b1
Kamil CAGLAR <kamilcaglar.contact@gmail.com>
2019-11-16 15:08:29 +0100

    refactor: remove accordion

--- src/main/java/fr/tse/ProjetInfo3/mvc/controller/UserTabController.java
+++ src/main/java/fr/tse/ProjetInfo3/mvc/controller/UserTabController.java
@@ -15,6 +15,7 @@ import javafx.scene.Scene;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TitledPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
@@ -64,6 +65,8 @@ public class UserTabController {
     @FXML
     private JFXSpinner progressIndicator;
 
+    @FXML
+    private TitledPane titledHashtag;
     @FXML
     private Pane thirdpane;
     /*
@@ -139,19 +142,18 @@ public class UserTabController {
     }
 
     /*
-    * Draws the profile picture after rounding it.
-    * */
+     * Draws the profile picture after rounding it.
+     * */
     private void buildPicture() {
         Image profilePic = new Image(userToPrint.getProfile_image_url_https());
         profileImageView.setImage(profilePic);
-        Circle clip = new Circle(67,67,67);
+        Circle clip = new Circle(67, 67, 67);
         profileImageView.setClip(clip);
         SnapshotParameters parameters = new SnapshotParameters();
         parameters.setFill(Color.TRANSPARENT);
         WritableImage image = profileImageView.snapshot(parameters, null);
         profileImageView.setClip(null);
         profileImageView.setImage(image);
-
     }
 
     @FXML
@@ -169,11 +171,6 @@ public class UserTabController {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
-    }
-
-    //This function prints a simplified version of user
-    private void setSimpleUser() {
-
     }
 
     private char getTypeSearch() {
@@ -218,6 +215,7 @@ public class UserTabController {
         }
         Platform.runLater(() -> {
             listHashtags.getItems().addAll(hashtagsToPrint);
+            titledHashtag.setMaxHeight(50*hashtagsToPrint.size());
             progressIndicator.setVisible(false);
         });
         return null;
