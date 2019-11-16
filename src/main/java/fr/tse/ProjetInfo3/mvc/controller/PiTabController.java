f439bcd35f5537cb3982d1049ac45619d7900ce1
Taha <taha.alami@um5s.net.ma>
2019-11-16 11:22:24 +0100

    feat : #12 created the controller for a single pi edition

--- src/main/java/fr/tse/ProjetInfo3/mvc/controller/PiTabController.java
+++ src/main/java/fr/tse/ProjetInfo3/mvc/controller/PiTabController.java
@@ -1,11 +1,36 @@
 package fr.tse.ProjetInfo3.mvc.controller;
 
-public class PiTabController {
-    private MainController mainController;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 
-    /*Controller can acces to this Tab */
+/**
+ * @author ALAMI IDRISSI Taha
+ * Controller of the Edit PI window, all user interactions whith the Edit PI windows (not the tabs) are handled here
+ * 
+ */
+public class PiTabController {
+
+    @FXML
+    private AnchorPane PIPane;
+
+    @FXML
+    private StackPane dialogStackPane;
+
+    @FXML
+    private AnchorPane anchorPane;
+    
+    private MainController mainController;
+    
+ 	/*Controller can acces to this Tab */
     public void injectMainController(MainController mainController) {
         this.mainController = mainController;
     }
-
+    
+    /*This function is launched when this tab is launched */
+    @FXML
+    private void initialize() {
+        
+    }
 }
+
