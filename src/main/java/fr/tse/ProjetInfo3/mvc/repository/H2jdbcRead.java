6e579cc9b9057ea85155e9340c7f9fed3645c264
Taha <taha.alami@um5s.net.ma>
2019-11-28 16:21:03 +0100

    feat : #52 reading data from the tables

--- src/main/java/fr/tse/ProjetInfo3/mvc/repository/H2jdbcRead.java
+++ src/main/java/fr/tse/ProjetInfo3/mvc/repository/H2jdbcRead.java
@@ -7,22 +7,18 @@ import java.sql.DriverManager;
 import java.sql.ResultSet; 
 import java.sql.SQLException; 
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.jfoenix.controls.JFXButton;
 
+import fr.tse.ProjetInfo3.mvc.dto.Hashtag;
 import javafx.fxml.FXML; 
 /**
  * @author Laïla
  *
  */
 public class H2jdbcRead {
-	   // JDBC driver name and database URL 
-	   static final String JDBC_DRIVER = "org.h2.Driver";   
-	   static final String DB_URL = "jdbc:h2:~/user";  
-	   
-	   //  Database credentials 
-	   static final String USER = "sa"; 
-	   static final String PASS = ""; 
 	   
 	   //JAVAFX
 	   @FXML
@@ -34,38 +30,34 @@ public class H2jdbcRead {
 	      Connection conn = null; 
 	      Statement stmt = null; 
 	      try { 
-	         // STEP 1: Register JDBC driver 
-	         Class.forName(JDBC_DRIVER); 
+	         
 	         
 	         // STEP 2: Open a connection 
 	         System.out.println("Connecting to database..."); 
-	         conn = DriverManager.getConnection(DB_URL,USER,PASS);  
+	         conn = SingletonDBConnection.getInstance();  
 	         
 	         // STEP 3: Execute a query 
 	         System.out.println("Connected database successfully..."); 
 	         stmt = conn.createStatement(); 
-	         String sql = "SELECT id, mail, twitter, password FROM userApp"; 
+	         String sql = "SELECT * FROM interestpoint"; 
 	         ResultSet rs = stmt.executeQuery(sql); 
 	         
 	         // STEP 4: Extract data from result set 
 	         while(rs.next()) { 
 	            // Retrieve by column name 
-	            int id  = rs.getInt("id"); 
-	            String mail = rs.getString("mail"); 
-	            String twitter = rs.getString("twitter"); 
-	            String password = rs.getString("password");  
+	            int id  = rs.getInt("interestpoint_id"); 
+	            String mail = rs.getString("name"); 
+	            String twitter = rs.getString("description"); 
+	            String password = rs.getString("created_at");  
 	            
-	            // Display values 
-	            System.out.print("ID: " + id); 
-	            System.out.print(", Mail: " + mail); 
-	            System.out.print(", Twitter: " + twitter); 
-	            System.out.println(", Password: " + password); 
+	            System.out.println("id : "+id);
+	            System.out.println("name : "+mail);
+	            System.out.println("description : "+twitter);
+	            System.out.println("created at : "+password);
+	            
 	         } 
 	         // STEP 5: Clean-up environment 
 	         rs.close(); 
-	      } catch(SQLException se) { 
-	         // Handle errors for JDBC 
-	         se.printStackTrace(); 
 	      } catch(Exception e) { 
 	         // Handle errors for Class.forName 
 	         e.printStackTrace(); 
@@ -81,6 +73,5 @@ public class H2jdbcRead {
 	            se.printStackTrace(); 
 	         } // end finally try 
 	      } // end try 
-	      System.out.println("Goodbye!"); 
 	   }
 }
