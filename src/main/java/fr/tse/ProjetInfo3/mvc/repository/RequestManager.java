13abc4d8f201d587a6138fbf93f6bd2fb90f63a5
Kamil CAGLAR <kamilcaglar.contact@gmail.com>
2019-11-28 09:15:53 +0100

    fix: show popular account first

--- src/main/java/fr/tse/ProjetInfo3/mvc/repository/RequestManager.java
+++ src/main/java/fr/tse/ProjetInfo3/mvc/repository/RequestManager.java
@@ -127,7 +127,7 @@ public class RequestManager {
      * @return Map of Names and screen_names of user
      */
     public Map<String, String> getUsersbyName(String userProposition) throws IOException, InterruptedException {
-        String url = "https://api.twitter.com/1.1/users/search.json?q=" + userProposition + "&count=20&include_entities=false";
+        String url = "https://api.twitter.com/1.1/users/search.json?q=" + userProposition + "&count=20&include_entities=false&result_type=popular";
 
         //if the proposition contains spaces we will remove them
         //WARNING ! we have to keep userProposition as it is because oAuthManager need spaces
