a71dd43dbe2cb9c20543a297cc52d7ede2af8d23
Kamil CAGLAR <kamilcaglar.contact@gmail.com>
2019-12-17 18:52:56 +0100

    feat: prepare Test Files

---
+++ src/test/java/fr/tse/ProjetInfo3/mvc/viewer/SearchViewerTest.java
@@ -0,0 +1,15 @@
+package fr.tse.ProjetInfo3.mvc.viewer;
+
+
+import org.junit.jupiter.api.Test;
+
+class SearchViewerTest {
+
+    @Test
+    void getListPropositions() {
+    }
+
+    @Test
+    void getUser() {
+    }
+}
